6c96eac841a6cc389e5f3063df035150daf43ab2
Slim Nate <slimnatekc@gmail.com>
2023-02-24 23:40:41 -0600

    configure build tools

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    require('postcss-nested'),
+    require('postcss-css-variables'),
+    require('postcss-utilities'),
+    require('postcss-custom-media'),
+  ],
+};
